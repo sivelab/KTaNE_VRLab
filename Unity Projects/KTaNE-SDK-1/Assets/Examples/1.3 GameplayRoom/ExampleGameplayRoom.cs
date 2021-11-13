6cf03bb3716135b24ceb18e2c26eabd9047459c4
Brendan Andrew Rood <rood0039@umn.edu>
2021-11-13 16:52:12 -0600

    Added a new scene and messed around with ExampleGameplayRoom.cs to see how lighting events work. Current state is a room that switches between red and blue each time a lighting event occurs

--- Unity Projects/KTaNE-SDK-1/Assets/Examples/1.3 GameplayRoom/ExampleGameplayRoom.cs
+++ Unity Projects/KTaNE-SDK-1/Assets/Examples/1.3 GameplayRoom/ExampleGameplayRoom.cs
@@ -13,6 +13,17 @@ public class ExampleGameplayRoom : MonoBehaviour
 
     public void OnLightChange(bool on)
     {
-        roomLight.enabled = on;
+        ticks++;
+        if(ticks == 1)
+        {
+            roomLight.color = new Color(1, 0, 0);
+        }
+        if (ticks == 2)
+        {
+            roomLight.color = new Color(0, 0, 1);
+            ticks = 0;
+        }
     }
+
+    private int ticks = 0;
 }
